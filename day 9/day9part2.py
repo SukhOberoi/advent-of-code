94dc10c74e6bb29c46f4b4aa6303dcc6b2383101
Sukh Oberoi <sukhoberoi24@gmail.com>
2023-12-09 16:04:33 +0530

    day 9

---
+++ day 9/day9part2.py
@@ -0,0 +1,25 @@
+def find_diff_seq(seqs):
+    new=[]
+    for a, b in zip(seqs[-1], seqs[-1][1:]):
+        new.append(b-a)
+    seqs.append(new)
+    return seqs
+
+with open("day 9\\input.txt", "r") as file:
+    data = file.readlines()
+    sumval=0
+    for line in data:
+        line = [list(map(lambda x: int(x), line.rstrip("\n").split()))]
+        while True:
+            add=line[-1][0]
+            if line[-1].count(add) == len(line[-1]):
+                depth = len(line)-2
+                while depth>=0:
+                    line[depth].insert(0, line[depth][0]-add)
+                    add = line[depth][0]
+                    depth-=1
+                sumval+=line[0][0]
+                break
+            else:
+                line = find_diff_seq(line)
+    print(sumval)
