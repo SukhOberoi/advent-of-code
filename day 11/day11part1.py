cb64e85c5761123c25689f267634fc7581f8ae74
Sukh Oberoi <sukhoberoi24@gmail.com>
2023-12-11 12:51:10 +0530

    day 11

---
+++ day 11/day11part1.py
@@ -0,0 +1,34 @@
+with open("day 11\\input.txt", "r") as file:
+    data = file.readlines()
+    data = list(map(lambda x: list(x.rstrip("\n")), data))
+    #expansion of universe
+    blank = [[], []]
+    for index, row in enumerate(data):
+        if '#' not in row:
+            blank[0].append(index)
+    for x in range(len(data[0])):
+        col=[]
+        for y in range(len(data)):
+            col.append(data[y][x])
+        if '#' not in col:
+            blank[1].append(x)
+    count =0
+    for x in blank[0]:
+        data.insert(x+count, ['.']*len(data[0]))
+        count+=1
+    count = 0
+    for x in blank[1]:
+        for y in range(len(data)):
+            data[y].insert(x+count, '.')
+        count+=1
+    galaxies = []
+    for y in range(len(data)):
+        for x in range(len(data[0])):
+            if data[y][x]=="#":
+                galaxies.append([y,x])
+    
+    from itertools import combinations
+    totaldistances = 0
+    for combo in combinations(galaxies, 2):
+        totaldistances+= abs(combo[0][0]- combo[1][0]) + abs(combo[0][1]- combo[1][1])
+    print(totaldistances)
